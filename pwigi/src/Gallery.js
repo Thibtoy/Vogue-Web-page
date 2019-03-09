5836fbdd7b3f5fe8aa15dfa4c82998f6ad8e1bd3
SolanshRocha <solansh.rp@icloud.com>
2019-03-09 17:05:23 +0100

    first commit

---
+++ pwigi/src/Gallery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from './Box';
+
+export default class Gallery extends React.Component {
+    render () {
+        return (
+            <div id="Gallery">
+                <Box></Box>
+            </div>
+            
+        )
+    }
+}
---
+++ pwigi/src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Nav extends React.Component {
+    render () {
+        return (
+            <ul id ="Nav">
+                <li>Home</li>
+                <li>News</li>
+                <li>Fashion</li>
+                <li>Art & Lifestyle</li>
+            </ul>
+        )
+    }
+}
