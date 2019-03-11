e8b917ade3753474da9fc15dd97ec6720d0316dc
Thibtoy <cabanes.thibault@gmail.com>
2019-03-11 12:04:18 +0100

    commit

--- pwigi/src/Box.js
+++ pwigi/src/Box.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 export default class Box extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			Tittle: [],
+			image: [], 
+			presentation: [],
+		}
+	}
+
+	componentWillMount() {
+		fetch('http://localhost:3000/')
+		.then(responses => {
+			console.log(responses);
+			return results.json();
+		}).then(data => {
+			let pictures = data.results.map((pic) => {
+				return (
+					<h2 key={pic.results}>{pic.articles_name}</h2>
+					)
+			})
+			this.setState( {Tittle: pictures});
+			console.log("tittle", this.state.Tittle)
+		})
+	}
+
     render () {
         return (
             <div className="Box">
-            <h2>test</h2>
-            <h5>test</h5>
+            	{this.state.Tittle}
             </div>
         )
     }
